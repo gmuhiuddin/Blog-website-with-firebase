d4fa69dc39d6cb5d648ff9cd0ed0473061cfc323
Ghulam_muhiuddin <muhiuddinghulam825@gmail.com>
2023-11-09 09:38:23 +0500

    added some work

---
+++ script.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDMeG-Yt8eUI3eoSEbLokIk9Fo_fCRTZ3k",
+    authDomain: "blog-app-9f834.firebaseapp.com",
+    projectId: "blog-app-9f834",
+    storageBucket: "blog-app-9f834.appspot.com",
+    messagingSenderId: "114009764949",
+    appId: "1:114009764949:web:3c7974840f125054e290dc",
+    measurementId: "G-K5QB7B6K9N"
+  };
+
+  const app = initializeApp(firebaseConfig);
